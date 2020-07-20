72421c7fa4f7ba3fad6687e12614fc22044d2a89
Myron <dubtwee@gmail.com>
2020-07-20 15:50:47 -0500

    adding checkout button to Inventory

--- src/components/pages/Inventory/Inventory.js
+++ src/components/pages/Inventory/Inventory.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class Inventory extends Component {
+
+  handleGoToCheckout = () => {
+    console.log('GOTO CHECKOUT');
+    this.props.history.push('/checkout');
+  }
+
   render() {
     return (
       <div className="container">
         <h2>Inventory Page</h2>
+        <button onClick={this.handleGoToCheckout}>Checkout NOW!!!</button>
       </div>
     );
   }
