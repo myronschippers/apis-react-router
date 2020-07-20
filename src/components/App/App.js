83186c8533b958d150839c2f95567f36c13af9b5
Myron <dubtwee@gmail.com>
2020-07-20 15:33:33 -0500

    registering page routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
+import Home from '../pages/Home/Home';
+import Inventory from '../pages/Inventory/Inventory';
+import Checkout from '../pages/Checkout/Checkout';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+      <Router>
+        <div>
+          <Header />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/inventory" component={Inventory} />
+          <Route path="/checkout" component={Checkout} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/Checkout/Checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Checkout Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
