d36ee55b62c575f088ffa2e037d81052acb956d3
Myron <dubtwee@gmail.com>
2020-07-20 15:44:03 -0500

    adding styling for navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './Header.css';
 
@@ -8,6 +9,14 @@ class Header extends Component {
       <header className="appBar">
         <img className="logo" src={logo} alt="logo" />
         <h1 className="primaryHdg">React Router (S.P.A.)</h1>
+
+        <nav className="nav">
+          <ul className="nav-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/inventory">Inventory</Link></li>
+            <li><Link to="/checkout">Checkout</Link></li>
+          </ul>
+        </nav>
       </header>
     );
   }
